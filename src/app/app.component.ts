29c02b4631c3f6de305261c788b79a8052bf9264
HVDevs <hvdevscompany@gmail.com>
2023-02-14 15:36:29 -0300

    14.02.2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mente-fit-landing-page';
+  ngOnInit() {
+    AOS.init({ once:true });
+    window.addEventListener('load', AOS.refresh);
+  }
 }
--- src/app/components/card-home/card-home.component.ts
+++ src/app/components/card-home/card-home.component.ts
@@ -11,17 +11,35 @@ export class CardHomeComponent {
   @Input("title") title!: string
   @ViewChild("left") left!: ElementRef
   @ViewChild("right") right!: ElementRef
+  dataAos: string = "fade-right"
 
+  /**
+  noOnInit(){
+    switch(this.container){
+      case 1:
+        this.dataAos = "fade-right"
+        break
+      case 2:
+        this.dataAos = "fade-left"
+        break
+      default:
+        this.dataAos = "fade-right"
+    }
+  }
+*/
   ngAfterViewInit(){
     switch(this.container){
       case 1:
         this.right.nativeElement.classList.add("gone")
+        this.dataAos = "fade-right"
         break
       case 2:
         this.left.nativeElement.classList.add("gone")
+        this.dataAos = "fade-left"
         break
       default:
         this.right.nativeElement.classList.add("gone")
+        this.dataAos = "fade-right"
     }
   }
 }
